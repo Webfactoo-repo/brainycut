ad8e5a8badb406ce2712f96143eb74009e0a1de5
lordcase <lordcase@gmail.com>
2014-11-22 21:42:58 +0100

    Edit - Add courses link to cart

--- wp-content/plugins/woocommerce/templates/cart/cart-empty.php
+++ wp-content/plugins/woocommerce/templates/cart/cart-empty.php
@@ -11,10 +11,19 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 wc_print_notices();
 
+if (is_user_logged_in()) {
+	$current_user = wp_get_current_user();
+	$href = icl_get_home_url() . 'members/' . $current_user->user_login . '/course/';
+	
+}else{
+	$href = icl_get_home_url() . 'all-courses/';
+}
+
 ?>
 
 <p class="cart-empty"><?php _e( 'Your cart is currently empty.', 'woocommerce' ) ?></p>
 
 <?php do_action( 'woocommerce_cart_is_empty' ); ?>
 
-<p class="return-to-shop"><a class="button wc-backward" href="<?php echo apply_filters( 'woocommerce_return_to_shop_redirect', get_permalink( wc_get_page_id( 'shop' ) ) ); ?>"><?php _e( 'Return To Shop', 'woocommerce' ) ?></a></p>
+<p class="return-to-shop"><a class="button wc-backward" href="<?php  echo $href;?>"><?php _e( 'Courses', 'vibe' ) ?></a></p>
+
