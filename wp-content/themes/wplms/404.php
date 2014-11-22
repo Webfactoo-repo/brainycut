7ed6161193dee72bd05145c6fa8fd79108f31f32
lordcase <lordcase@gmail.com>
2014-11-22 21:43:59 +0100

    Edit - added multilang func to 404

--- wp-content/themes/wplms/404.php
+++ wp-content/themes/wplms/404.php
@@ -3,7 +3,7 @@ if(is_404()){
 	$error404 = vibe_get_option('error404');
     if(isset($error404)){
         $page_id=  intval($error404);
-        wp_redirect( get_permalink( $page_id ),301); 
+        wp_redirect( get_permalink( icl_object_id($page_id, "page", false,ICL_LANGUAGE_CODE) ),301); 
         exit;
     }
     
